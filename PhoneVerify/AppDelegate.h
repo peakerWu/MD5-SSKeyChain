0a7e4dc567745d72061e14b5aec94ceea93eb72c
peaker <740971721@qq.com>
2016-04-15 14:16:14 +0800

    Initial Commit

---
+++ PhoneVerify/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhoneVerify
+//
+//  Created by dev on 16/4/15.
+//  Copyright © 2016年 donglian@eastunion.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhoneVerify/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PhoneVerify
+//
+//  Created by dev on 16/4/15.
+//  Copyright © 2016年 donglian@eastunion.net. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
