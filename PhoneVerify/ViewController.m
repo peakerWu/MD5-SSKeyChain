ff5beddf3e7411eb810512898febaa97492ea87c
peaker <740971721@qq.com>
2016-04-25 18:08:36 +0800

    加入md5

--- PhoneVerify/ViewController.m
+++ PhoneVerify/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import <SSKeychain.h>
+#import <CommonCrypto/CommonDigest.h>
 
 NSString *const serviceName = @"net.eastunion";
 //NSString *const account = @"你好";
@@ -84,5 +85,55 @@ NSString *const serviceName = @"net.eastunion";
 {
     [self.view endEditing:YES];
 }
+
+//- (NSString *)md5:(NSString *)str{
+//    // 转化成utf-8
+//    const char *cStr=[str UTF8String];
+//    // 开辟一个16字节(128位:MD5加密出来就是128位/bit(字节)，1字节=8位)的空间
+//    unsigned char result[16];
+//    // 加密存储到result中，将cStr字符串转换成了32位，一般来说，此过程不可逆，即只能加密，不能解密
+//    CC_MD5(cStr, strlen(cStr), result);
+//    // 其中%02x是格式控制符：‘x’表示以16进制输出，‘02’表示不足两位，前面补0
+//    return [NSString stringWithFormat:@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+//            result[0], result[1], result[2], result[3],
+//            result[4], result[5], result[6], result[7],
+//            result[8], result[9], result[10], result[11],
+//            result[12], result[13], result[14], result[15]];
+//}
+
+// md5加密另一种写法：利用for循环输出
+- (NSString *)md5:(NSString *)str{
+    // 转化成utf-8
+    const char *cStr=[str UTF8String];
+    // 开辟一个16字节(128位:MD5加密出来就是128位/bit(字节)，1字节=8位)的空间
+    unsigned char result[16];
+    // 加密存储到result中，将cStr字符串转换成了32位
+    CC_MD5(cStr, strlen(cStr), result);
+    // 其中%02x是格式控制符：‘x’表示以16进制输出，‘02’表示不足两位，前面补0
+    NSMutableString *mStr=[NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH];
+    for (int i=0; i<CC_MD5_DIGEST_LENGTH; i++) {
+        [mStr appendFormat:@"%02X",result[i]];
+    }
+    return mStr;
+}
+
+// sha1加密
+- (NSString *)sha1:(NSString *)str{
+    const char *cStr=[str cStringUsingEncoding:NSUTF8StringEncoding];
+    NSData *data=[NSData dataWithBytes:cStr length:str.length];
+    
+    uint8_t disgest[CC_SHA1_DIGEST_LENGTH];
+    
+    CC_SHA1(data.bytes, data.length, disgest);
+    
+    NSMutableString *output=[NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH*2];
+    
+    for (int i=0; i<CC_SHA1_DIGEST_LENGTH; i++) {
+        [output appendFormat:@"%02x",disgest[i]];
+    }
+    
+    return output;
+}
+
 
 @end
